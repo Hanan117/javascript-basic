f511a13e42b3868f4f9de0d6176473d26de13928
hanan117 <ashrawyhanan1117@gmail.com>
2026-07-28 14:44:28 +0700

    menyelesaikan tugas latihan-5

---
+++ bab-03-variable/latihan-5/index5.js
@@ -0,0 +1,15 @@
+console.log("====MAPEL===");
+
+const nilaiMatematika = 85;
+const nilaiIPA = 90;
+const nilaiBahasaIndonesia = 80;
+
+const totalNilai = nilaiMatematika + nilaiIPA + nilaiBahasaIndonesia;
+const jumlahPelajaran = 3;
+const rataRata = totalNilai / jumlahPelajaran;
+
+console.log(`Nilai Matematika: ${nilaiMatematika}`);
+console.log(`Nilai IPA: ${nilaiIPA}`);
+console.log(`Nilai Bahasa Indonesia: ${nilaiBahasaIndonesia}`);
+console.log("----------------------------------------");
+console.log(`Rata-rata nilai Anda adalah: ${rataRata}`);
