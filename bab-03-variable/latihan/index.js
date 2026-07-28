cb226e7045f373fbaac6cbbea1e53d02c14a3d42
hanan117 <ashrawyhanan1117@gmail.com>
2026-07-28 10:49:45 +0700

    menambahkan jawaban latihan (1)

---
+++ bab-03-variable/latihan/index.js
@@ -0,0 +1,11 @@
+console.log("=====BIODATA=====");
+
+const namaLengkap = "Hanan";
+let umur = 24;
+let kota = "Yogyakarta";
+let pekerjaan = "Ai Engineer";
+
+console.log("nama:", namaLengkap);
+console.log("umur:", umur);
+console.log("kota:", kota);
+console.log("pekerjaan:", pekerjaan);
