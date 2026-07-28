67e8f65332b98a8fbc7035911e1e3fb7c019ccd7
hanan117 <ashrawyhanan1117@gmail.com>
2026-07-28 15:11:03 +0700

    menyelesaikan tugas challenge-2

---
+++ bab-03-variable/challenge-2/indexcal2.js
@@ -0,0 +1,15 @@
+console.log("====MY====")
+
+const namaLengkap = "Hanan";
+const umur = 24;
+const kotaAsal = "Yogyakarta";
+const alamatEmail = "hanan.a@email.com";
+const hobiUtama = "Futsal dan Coding";
+const citaCita = "Software Engineer Profesional";
+
+console.log(`Nama Lengkap : ${namaLengkap}`);
+console.log(`Umur         : ${umur} tahun`);
+console.log(`Kota         : ${kotaAsal}`);
+console.log(`Email        : ${alamatEmail}`);
+console.log(`Hobi         : ${hobiUtama}`);
+console.log(`Cita-cita    : ${citaCita}`);
