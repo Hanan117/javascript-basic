a6500efe472f9ccd503e2fb6f08602d5a7754f6c
hanan117 <ashrawyhanan1117@gmail.com>
2026-07-28 14:15:56 +0700

    menyelesaikan latihan-3

---
+++ bab-03-variable/latihan-3/index3.js
@@ -0,0 +1,12 @@
+console.log("====Variable 3====")
+
+let namaProduk = "baju Batik";
+let hargaSatuan = 350000;
+let jumlahDibeli = 2;
+
+let totalHarga = hargaSatuan * jumlahDibeli;
+
+console.log("Produk : " + namaProduk);
+console.log("Harga : " + hargaSatuan);
+console.log("Jumlah : " + jumlahDibeli);
+console.log("Total : " + totalHarga);
