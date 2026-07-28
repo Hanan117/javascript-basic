57b03734001421931e65a96c6cc345a8cc34a06b
hanan117 <ashrawyhanan1117@gmail.com>
2026-07-28 14:31:39 +0700

    menyelesaikan pertemuan-4

---
+++ bab-03-variable/latihan-4/index4.js
@@ -0,0 +1,12 @@
+console.log("===Umur===")
+
+
+const nama = "Budi";
+const tahunLahir = 2002;
+const tahunSekarang = 2026;
+
+const umur = tahunSekarang - tahunLahir;
+
+console.log(`Halo, nama saya ${nama}.`);
+console.log(`Saya lahir pada tahun ${tahunLahir}.`);
+console.log(`Umur saya sekarang adalah ${umur} tahun.`);
