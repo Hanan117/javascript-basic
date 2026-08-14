b90094e61ea680c793b8899aab66fb55e7772d67
hanan117 <ashrawyhanan1117@gmail.com>
2026-08-14 07:55:05 +0700

    menyelesaikan bab 14

--- bab-14-date-object/LATIHAN/soal15.js
+++ bab-14-date-object/LATIHAN/soal15.js
@@ -2,7 +2,7 @@ const sekarang = new Date("2026-08-13");
 
 const tugas = [
   { nama: "Membuat Website Portfolio", deadline: "2026-08-20" },
-  { nama: "Mengerjakan Latihan JavaScript", deadline: "2026-08-10" }, // Tanggal sudah lewat dari tanggal 13
+  { nama: "Mengerjakan Latihan JavaScript", deadline: "2026-08-10" }, 
   { nama: "Presentasi Project", deadline: "2026-09-01" },
 ];
 
@@ -11,18 +11,18 @@ console.log("===== DAFTAR TUGAS =====\n");
 tugas.forEach((item, index) => {
   const tanggalDeadline = new Date(item.deadline);
 
-  // Menghitung selisih hari menggunakan getTime()
+  
   const selisihMs = tanggalDeadline.getTime() - sekarang.getTime();
   const selisihHari = Math.round(selisihMs / (1000 * 60 * 60 * 24));
+  
 
-  // Format tanggal deadline
   const formatDeadline = tanggalDeadline.toLocaleDateString("id-ID", {
       day: "numeric",
       month: "long",
       year: "numeric"
   });
 
-  // Menentukan status
+ 
   let status;
   if (selisihHari > 0) {
       status = `${selisihHari} hari lagi`;
@@ -30,7 +30,7 @@ tugas.forEach((item, index) => {
       status = "Deadline sudah terlewat.";
   }
 
-  // Menampilkan hasil
+  
   console.log(`${index + 1}. ${item.nama}`);
   console.log(`   Deadline: ${formatDeadline}`);
   console.log(`   Status  : ${status}\n`);
